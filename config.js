74e9add3fd17c5ae99bb377d404edee373d0acc6
dzulfathan11-dev <dzulfathan11@gmail.com>
2026-09-18 15:09:58 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,29 @@
+// config.js
+const portfolioConfig = {
+    Nama: "ZUL FATHAN FIRDAUS JUNIAR",
+    Nama Pangilan: "FATHAN/ZUL",
+    sekolah: "SMK BHINEKA KARYA SIMO",
+    kelas: "X",
+    Jurusan: "Teknik Komputer Jaringan",
+    Kota: "Boyolali",
+    status: "Pelajar",
+    profileImage: "assets/images/profile.jpg", // Pastikan gambar ada di folder ini
+    
+    // Teks untuk section Home
+    heroBio: "BOCIL PUBG",
+    
+    // Teks untuk section About
+    about: "mbuh",
+    quote: "mbuh",
+    
+    // Data tambahan untuk About section
+    interests: ["op ae"],
+    goals: ["opo ae"],
+    
+    // Link Sosial Media (kosongkan '#' jika tidak ada)
+    social: {
+        instagram: "@dzul_fathan",
+        github: "dzullfathan11",
+        tiktok: "@dzul_fathan",
+    }
+};
